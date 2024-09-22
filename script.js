81fcec3d8512c4a468f81fb60e5ee9d622be2d78
NuttyShooter <111531396+NuttyShooter@users.noreply.github.com>
2024-09-22 23:02:03 +0530

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+// Flip effect for project cards
+const cards = document.querySelectorAll(".card__inner");
+
+cards.forEach(card => {
+  card.addEventListener("click", function () {
+    card.classList.toggle('is-flipped');
+  });
+});
